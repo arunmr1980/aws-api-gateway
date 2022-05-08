55dc195e68346dac11ec221bee1ead077abb4df1
Arun MR <arun_mr549e@protonmail.com>
2022-05-08 12:25:12 +0530

    device schema updated

--- auth-service/db/db.sql
+++ auth-service/db/db.sql
@@ -1,11 +1,22 @@
 
-create table User (
+create table rs_user (
   id int unsigned auto_increment primary key,
-  useraccountkey varchar(255) not null unique,
+  user_account_key varchar(255) not null unique,
   email varchar(255) not null,
   mobile varchar(255) not null,
   username varchar(255) not null unique,
   firstname varchar(255) not null,
   lastname varchar(255),
-  password varchar(1023) not null,
-  authtoken varchar(1023) );
+  password varchar(1023) not null);
+
+  create table rs_user_device (
+    user_account_key varchar(255) not null,
+    device_key varchar(255) not null unique,
+    device_name varchar(255) not null,
+    access_token varchar(1023),
+    refresh_token varchar(1023),
+    access_token_expiry_datetime bigint unsigned,
+    refresh_token_expiry_datetime bigint unsigned
+  );
+
+  alter table rs_user_device add constraint PK_Device primary key(user_account_key, device_key);
